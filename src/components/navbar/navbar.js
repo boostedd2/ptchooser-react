652ceb4dd0e2d891b53daa9dbf7d4c4913ed51f6
boost <maypojr@gmail.com>
2020-01-03 22:06:13 -0500

    init

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Navdrawer from '../drawer/drawer'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return(
+    <div className={classes.root}>
+      <AppBar position="static" style={{backgroundColor: "#212121"}}>
+        <Toolbar>
+          <Navdrawer />
+          <h2>PTChooser</h2>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default Navbar;
