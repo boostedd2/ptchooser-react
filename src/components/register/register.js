17d17470797899aaa5fd10e55e32eab9a5228204
luke <luke@bytewise.com>
2020-01-29 19:32:03 -0500

    manage users

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import ClosedBeta from '../misc/closedBeta';
+import axios from 'axios';
+
+//input sanitize
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+//dev toggle
+const dev = true
+let url
+
+if (dev === false) {
+  url = "https://www.hdjfygr.xyz"
+} else {
+  url = "http://192.168.1.17:8000"
+}
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,6 +39,9 @@ const useStyles = makeStyles(theme => ({
   entryField: {
     marginBottom: "10px",
     background: "grey",
+    [theme.breakpoints.up('md')]: {
+      width: "40%",
+    },
   },
   submitButton: {
     color:"white",
@@ -46,24 +68,67 @@ const CssTextField = withStyles({
   },
 })(TextField);
 
-const closedbeta = true;
+const closedbeta = false;
 
 const Register = () => {
   const classes = useStyles();
+  let history = useHistory()
+  const [createUserName, setCreateUserName] = useState('')
+  const [createUserPassword, setCreateUserPassword] = useState('')
+  const [confirmUserPassword, setConfirmUserPassword] = useState('')
+
+  const inputCreateUser = function(event) {
+    setCreateUserName(event.target.value);
+  }
+
+  const inputCreateUserPassword = function(event) {
+    setCreateUserPassword(event.target.value);
+  }
+
+  const inputConfirmUserPassword = function(event) {
+    setConfirmUserPassword(event.target.value);
+  }
+
+  const registerSubmit = () => {
+    if (confirmUserPassword === createUserPassword) {
+      const postData = {
+        "username": DOMPurify.sanitize(createUserName),
+        "password": DOMPurify.sanitize(createUserPassword)
+      }
+      axios.post(
+        url + "/users/register", postData
+      )
+      .then(res => {
+        alert("Account created, please await moderator approval before using.")
+        return history.push('/')
+      })
+      .catch(err => {
+        const errMessage = err.response.data
+        document.getElementById("error").innerHTML = errMessage
+        document.getElementById("error").style.display = "block"
+        setTimeout(function(){ document.getElementById("error").style.display = "none" }, 3000);
+      })
+    } else {
+        const errMessage = "Passwords do not match!"
+        document.getElementById("error").innerHTML = errMessage
+        document.getElementById("error").style.display = "block"
+        setTimeout(function(){ document.getElementById("error").style.display = "none" }, 3000);
+    }
+  }
 
   return(
     <div className={classes.root}>
       <h1>Register</h1>
       {closedbeta ? <div className={classes.loading}><ClosedBeta /></div> :
       <div>
-        <h3>All sign ups must be approved, you will receive an email when your account is created.</h3>
+        <h3>All sign ups must be approved, a moderator will review your account for approval and notify you usually within 48 hours.</h3>
         <h3>NOTE: An account is not needed to view weapon stacks.</h3>
+        <h3 id="error" style={{color: "red", display: "none"}}>.</h3>
         <form className={classes.registerForm}>
-        <CssTextField className={classes.entryField} id="filled-basic" label="Email" variant="filled" />
-        <CssTextField className={classes.entryField} id="filled-basic" label="Username" variant="filled" />
-        <CssTextField className={classes.entryField} id="filled-basic" label="Create Password" variant="filled" />
-        <CssTextField className={classes.entryField} id="filled-basic" label="Confirm Password" variant="filled" />
-        <Button className={classes.submitButton} size="small">Submit</Button>
+          <CssTextField className={classes.entryField} id="filled-basic" label="Username" variant="filled" onChange={inputCreateUser} />
+          <CssTextField className={classes.entryField} id="filled-basic" label="Create Password" variant="filled" type="password" onChange={inputCreateUserPassword} />
+          <CssTextField className={classes.entryField} id="filled-basic" label="Confirm Password" variant="filled" type="password" onChange={inputConfirmUserPassword} />
+          <Button className={classes.submitButton} size="small" onClick={() => registerSubmit()}>Submit</Button>
         </form>
       </div>}
     </div>
